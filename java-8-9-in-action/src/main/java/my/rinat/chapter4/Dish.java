d5633caaada9bf508b7e4811ea5de3db101726f8
Rinat Zalyaletdinov <rinat_zalyaletdinov@epam.com>
2017-12-06 20:25:08 +0300

    Create Dish entity

---
+++ java-8-9-in-action/src/main/java/my/rinat/chapter4/Dish.java
@@ -0,0 +1,57 @@
+package my.rinat.chapter4;
+
+import java.util.Arrays;
+import java.util.List;
+
+@SuppressWarnings("unused")
+public class Dish {
+
+    public enum Type {
+        MEAT, FISH, OTHER
+    }
+
+    private final String name;
+    private final boolean vegeterian;
+    private final int calories;
+    private final Type type;
+
+    private Dish(String name, boolean vegeterian, int calories, Type type) {
+        this.name = name;
+        this.vegeterian = vegeterian;
+        this.calories = calories;
+        this.type = type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public boolean isVegeterian() {
+        return vegeterian;
+    }
+
+    public int getCalories() {
+        return calories;
+    }
+
+    public Type getType() {
+        return type;
+    }
+
+    @Override
+    public String toString() {
+        return this.name;
+    }
+
+    public static final List<Dish> menu = Arrays.asList(
+            new Dish("pork", false, 800, Dish.Type.MEAT),
+            new Dish("beef", false, 700, Dish.Type.MEAT),
+            new Dish("chicken", false, 400, Dish.Type.MEAT),
+            new Dish("french fries", true, 530, Dish.Type.OTHER),
+            new Dish("rice", true, 350, Dish.Type.OTHER),
+            new Dish("season fruit", true, 120, Dish.Type.OTHER),
+            new Dish("pizza", true, 550, Dish.Type.OTHER),
+            new Dish("prawns", false, 300, Dish.Type.FISH),
+            new Dish("salmon", false, 450, Dish.Type.FISH)
+    );
+}
