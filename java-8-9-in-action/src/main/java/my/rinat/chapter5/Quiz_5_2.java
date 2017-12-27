954095ff91c66d5084f36b2c5d491c8427a98887
Rinat Zalyaletdinov <rinat_zalyaletdinov@epam.com>
2017-12-27 14:00:42 +0500

    Implement Quiz 5.2 (exercise 2)

--- java-8-9-in-action/src/main/java/my/rinat/chapter5/Quiz_5_2.java
+++ java-8-9-in-action/src/main/java/my/rinat/chapter5/Quiz_5_2.java
@@ -3,9 +3,17 @@ package my.rinat.chapter5;
 import java.util.Arrays;
 
 class Quiz_5_2 {
+
     static Long[] squaresArray(Long[] numbers) {
         return Arrays.stream(numbers)
                 .map(number -> number * number)
                 .toArray(Long[]::new);
+    }
+
+    static Long[][] allNumberPairs(Long[] numbers1, Long[] numbers2) {
+        return Arrays.stream(numbers1)
+                .flatMap(number1 -> Arrays.stream(numbers2)
+                        .map(number2 -> new Long[]{number1, number2}))
+                .toArray(Long[][]::new);
     }
 }
--- java-8-9-in-action/src/test/java/my/rinat/chapter5/Quiz_5_2Test.java
+++ java-8-9-in-action/src/test/java/my/rinat/chapter5/Quiz_5_2Test.java
@@ -3,6 +3,8 @@ package my.rinat.chapter5;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.Arrays;
+
 public class Quiz_5_2Test {
 
     @Test
@@ -16,5 +18,21 @@ public class Quiz_5_2Test {
 
         // Then
         Assert.assertArrayEquals(expectedSquares, actualSquares);
+    }
+
+    @Test
+    public void shouldReturnAllNumberPairsArray() {
+        // Given
+        Long[] numbers1 = {1L, 2L, 3L};
+        Long[] numbers2 = {3L, 4L};
+        Long[][] expectedPairs = {{1L, 3L}, {1L, 4L}, {2L, 3L}, {2L, 4L}, {3L, 3L}, {3L, 4L}};
+
+        // When
+        Long[][] actualPairs = Quiz_5_2.allNumberPairs(numbers1, numbers2);
+
+        // Then
+        System.out.println(Arrays.deepToString(expectedPairs));
+        System.out.println(Arrays.deepToString(actualPairs));
+        Assert.assertArrayEquals(expectedPairs, actualPairs);
     }
 }
