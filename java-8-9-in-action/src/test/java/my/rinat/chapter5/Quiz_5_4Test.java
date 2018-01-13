522fc11a844e96561a2a792a5aaee3554c175e86
Rinat Zalyaletdinov <rinat_zalyaletdinov@epam.com>
2018-01-13 16:04:41 +0300

    Implement Quiz 5.4

---
+++ java-8-9-in-action/src/test/java/my/rinat/chapter5/Quiz_5_4Test.java
@@ -0,0 +1,31 @@
+package my.rinat.chapter5;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+public class Quiz_5_4Test {
+
+    private final ByteArrayOutputStream output = new ByteArrayOutputStream();
+
+    @Before
+    public void setUp() {
+        System.setOut(new PrintStream(output));
+    }
+
+    @After
+    public void tearDown() {
+        System.setOut(null);
+    }
+
+    @Test
+    public void shouldPrintFibonacciTuples() {
+        Quiz_5_4.printFibonacciTuples(8);
+        Assert.assertEquals("(0,1)\r\n(1,1)\r\n(1,2)\r\n(2,3)\r\n(3,5)\r\n(5,8)\r\n(8,13)\r\n(13,21)\r\n",
+                output.toString());
+    }
+}
