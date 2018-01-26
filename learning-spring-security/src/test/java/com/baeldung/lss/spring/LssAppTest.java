febb3f01ec4f5df93759d2f43f794eed470e97be
Rinat_Zalyaletdinov <Rinat_Zalyaletdinov@epam.com>
2018-01-26 13:54:20 +0300

    Use spring-boot bom as in the course

---
+++ learning-spring-security/src/test/java/com/baeldung/lss/spring/LssAppTest.java
@@ -0,0 +1,16 @@
+package com.baeldung.lss.spring;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+public class LssAppTest {
+
+    @Test
+    public void test() {
+    }
+}
