347494a0394aa6a31de2f5152b6a1f65cef7b2a5
Rinat_Zalyaletdinov <Rinat_Zalyaletdinov@epam.com>
2018-01-30 16:46:58 +0300

    Implement web app security

---
+++ servlets-and-jsps-web/src/main/java/my/rinat/servlet/HelloWorldServlet.java
@@ -0,0 +1,17 @@
+package my.rinat.servlet;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class HelloWorldServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/pages/hello-world.jsp");
+        dispatcher.forward(request, response);
+    }
+}
---
+++ servlets-and-jsps-web/src/main/webapp/WEB-INF/pages/hello-world.jsp
@@ -0,0 +1,11 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Hello World</title>
+</head>
+<body>
+
+<h3>"Hello World" JSP!</h3>
+
+</body>
+</html>
