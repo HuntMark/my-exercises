109001c0f1555d33d21ebf7458b94bd5980eeac7
Rinat_Zalyaletdinov <Rinat_Zalyaletdinov@epam.com>
2018-01-31 13:33:54 +0300

    LSS: Update project sources to version lss-m1-l6

--- learning-spring-security/src/main/java/com/baeldung/lss/spring/LssSecurityConfig.java
+++ learning-spring-security/src/main/java/com/baeldung/lss/spring/LssSecurityConfig.java
@@ -9,25 +9,31 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @EnableWebSecurity
 public class LssSecurityConfig extends WebSecurityConfigurerAdapter {
 
+    public LssSecurityConfig() {
+        super();
+    }
+
+    //
+
     @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception { // @formatter:off
-        auth
-            .inMemoryAuthentication()
-            .withUser("user").password("pass").roles("USER");
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception { // @formatter:off 
+        auth.
+            inMemoryAuthentication().
+            withUser("user").password("pass").
+            roles("USER");
     } // @formatter:on
 
     @Override
     protected void configure(HttpSecurity http) throws Exception { // @formatter:off
         http
+        .authorizeRequests()
+                .anyRequest().authenticated()
+        
+        .and()
+        .formLogin().
+            loginPage("/login").permitAll().
+            loginProcessingUrl("/doLogin")
+        ;
+    }
 
-            .authorizeRequests()
-                .antMatchers("/delete/**").hasRole("ADMIN")
-				.anyRequest().authenticated()
-
-            .and()
-
-            .formLogin()
-                .loginPage("/login").permitAll()
-                .loginProcessingUrl("/doLogin");
-    } // @formatter:on
 }
